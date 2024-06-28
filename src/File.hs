2067fa4fb6bfb590e0fcfc62e6eaf8861f7c0f03
Nicolas Girot <ngirot@hotmail.com>
2024-06-28 23:00:19 +0200

    Fix File loading to be more precise on the reason the file is not loaded

--- src/File.hs
+++ src/File.hs
@@ -4,11 +4,16 @@ module File (loadFileContent, ConfigurationError (..)) where
 
 import Control.Arrow
 import Control.Exception
+import System.Directory
 
-data ConfigurationError = FileDoesNotExists
+data ConfigurationError = FileDoesNotExists | FileNotReadable
   deriving (Eq, Show)
 
 loadFileContent :: String -> IO (Either ConfigurationError String)
-loadFileContent fileName = fmap (left mapError) (try @IOError $ readFile fileName)
+loadFileContent fileName = do
+  exists <- doesFileExist fileName
+  if exists
+    then fmap (left mapError) (try @IOError $ readFile fileName)
+    else pure $ Left FileDoesNotExists
   where
-    mapError _ = FileDoesNotExists
+    mapError _ = FileNotReadable
