09d30a8703aa76aa542934ca1c6f67484a373068
Nicolas Girot <ngirot@hotmail.com>
2024-07-07 21:32:45 +0200

    Add integration test

---
+++ test/LibSpec.hs
@@ -0,0 +1,36 @@
+module LibSpec where
+
+import Config
+import Control.Concurrent
+import qualified Data.ByteString as S
+import Lib
+import Network
+import Network.Socket.ByteString (recv, sendAll)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  connect
+
+launchTest :: Expectation
+launchTest = do
+  let configuration = ServerConfiguration "localhost" 3232
+  forkIO $ serve configuration
+  threadDelay 50000
+  r <- runTCPClient "localhost" "3232" $ \socket -> do
+    sendAll socket $ S.pack [5, 1, 0]
+    aaa <- S.unpack <$> recv socket 4096
+    if aaa /= [5, 0]
+      then return $ aaa `shouldBe` [5, 0]
+      else do
+        sendAll socket $ S.pack [5, 1, 0, 1, 216, 58, 215, 36, 0, 80]
+        bbb <- S.unpack <$> recv socket 4096
+        pure $ bbb `shouldBe` [5, 0, 0, 1, 216, 58, 215, 36, 0, 80]
+  case r of
+    Right x -> x
+    Left _ -> False `shouldBe` True
+
+connect :: Spec
+connect =
+  describe "Simple connect request" $ do
+    it "Should send request" $ launchTest
