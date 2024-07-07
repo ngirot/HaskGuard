1052377e4fa546474e334504e4c95c6a387f18b1
Nicolas Girot <ngirot@hotmail.com>
2024-07-07 17:11:56 +0200

    Reduce Main size

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,12 @@
 module Main (main) where
 
-import Config
-import Control.Concurrent (forkIO)
-import qualified Data.ByteString as S
+import Lib (serve)
 import Loader
-import Lib (negotiate, request)
-import Network
-import Network.Socket.ByteString (recv, sendAll)
-import Streaming (stream)
-import Errors
 
 main :: IO ()
 main = do
   loadedConf <- load
   case loadedConf of
-    Right conf -> do
-      putStrLn $ "Start listening " ++ scListen conf ++ ":" ++ show (scPort conf) ++ "..."
-      runTCPServer (Just $ scListen conf) (show $ scPort conf) talk
+    Right conf -> serve conf
     Left (ConfigurationNotAccessible e) -> putStrLn $ "Unable to load configuration " ++ show e
     Left (BadConfiguration e) -> putStrLn $ "Bad configuration " ++ show e
-  where
-    onConnect s r ss = do
-      sendAll s $ S.pack r
-      _ <- forkIO $ stream s ss
-      stream ss s
-      putStrLn ">>> Completed"
-      
-    afterNego s d = do
-      sendAll s $ S.pack d
-
-      putStrLn ">>> Request"
-      msgRequest <- recv s 1024
-      print $ S.unpack msgRequest
-      req <- request (S.unpack msgRequest) (onConnect s)
-      case req of
-        Right _ -> putStrLn "Ok"
-        Left (NoResponseError err) -> putStrLn $ "Error: " ++ err
-        Left (ResponseError response) -> sendAll s $ S.pack response
-    talk s = do
-      putStrLn ">>> Negotiation"
-      msgNegociation <- recv s 1024
-      print $ S.unpack msgNegociation
-
-      let nego = negotiate $ S.unpack msgNegociation
-      case nego of
-        Right d -> afterNego s d
-        Left (NoResponseError err) -> putStrLn $ "Error: " ++ err
-        Left (ResponseError response) -> sendAll s $ S.pack response
--- from the "network-run" package.
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,51 @@
-module Lib (negotiate, request) where
+module Lib (serve) where
 
+import Config
+import Control.Concurrent (forkIO)
+import qualified Data.ByteString as S
 import Data.Word (Word8)
 import Errors
 import Negotiation (generateNegotiationOutput2)
+import Network
 import Network.Socket
+import Network.Socket.ByteString (recv, sendAll)
 import Request
+import Streaming (stream)
+
+serve :: ServerConfiguration -> IO ()
+serve configuration = do
+  putStrLn $ "Start listening " ++ scListen configuration ++ ":" ++ show (scPort configuration) ++ "..."
+  runTCPServer (Just $ scListen configuration) (show $ scPort configuration) talk
+  where
+    onConnect s r ss = do
+      sendAll s $ S.pack r
+      _ <- forkIO $ stream s ss
+      stream ss s
+      putStrLn ">>> Completed"
+
+    afterNego s d = do
+      sendAll s $ S.pack d
+
+      putStrLn ">>> Request"
+      msgRequest <- recv s 1024
+      print $ S.unpack msgRequest
+      req <- request (S.unpack msgRequest) (onConnect s)
+      case req of
+        Right _ -> putStrLn "Ok"
+        Left (NoResponseError err) -> putStrLn $ "Error: " ++ err
+        Left (ResponseError response) -> sendAll s $ S.pack response
+    talk s = do
+      putStrLn ">>> Negotiation"
+      msgNegociation <- recv s 1024
+      print $ S.unpack msgNegociation
+
+      let nego = negotiate $ S.unpack msgNegociation
+      case nego of
+        Right d -> afterNego s d
+        Left (NoResponseError err) -> putStrLn $ "Error: " ++ err
+        Left (ResponseError response) -> sendAll s $ S.pack response
+
+-- from the "network-run" package.
 
 negotiate :: [Word8] -> Either RequestError [Word8]
 negotiate payload = generateNegotiationOutput2 payload
