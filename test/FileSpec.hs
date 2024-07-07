513251a92c5b3af07f843039933946357a3158d3
Nicolas Girot <ngirot@hotmail.com>
2024-07-07 13:37:03 +0200

    Wording

--- test/FileSpec.hs
+++ test/FileSpec.hs
@@ -1,4 +1,4 @@
-module FileSpec(spec) where
+module FileSpec (spec) where
 
 import File
 import Test.Hspec
@@ -11,4 +11,4 @@ loadFile :: Spec
 loadFile =
   describe "Load file" $ do
     it "Should load file content" $ loadFileContent "test/test.file" `shouldReturn` Right "content of the file"
-    it "Should load file content" $ loadFileContent "not-existing-file" `shouldReturn` Left FileDoesNotExists
+    it "Should have an error when file does not exists" $ loadFileContent "not-existing-file" `shouldReturn` Left FileDoesNotExists
