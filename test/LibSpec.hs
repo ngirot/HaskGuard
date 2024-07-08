e63259cbceda6670f55d3cb7aab8cc6c65211607
Nicolas Girot <ngirot@hotmail.com>
2024-07-08 17:35:23 +0200

    Start and use a fake server for integration test to have a full proxy communication

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -21,14 +21,17 @@ spec :: Spec
 spec = do
   connect
 
-launchTest :: [Communication] -> Expectation
-launchTest communications = do
+launchTest :: Int -> [Communication] -> Expectation
+launchTest port communications = do
   serverPort <- getFreePort
   let configuration = ServerConfiguration "localhost" serverPort
   signal <- newEmptyMVar
+  signal2 <- newEmptyMVar
+  _ <- forkIO $ runTCPServer (Just "127.0.0.1") (show port) (putMVar signal2 True) mult2Server
   _ <- forkIO $ serve configuration (\_ -> return ()) (putMVar signal True)
-  started <- takeMVar signal
-  if started
+  fakeTargetStarted <- takeMVar signal2
+  serverStarted <- takeMVar signal
+  if serverStarted && fakeTargetStarted
     then do
       r <- runTCPClient "localhost" (show serverPort) $ \socket -> do
         forM_ communications (reduceCommunication socket)
@@ -36,14 +39,31 @@ launchTest communications = do
       case r of
         Right v -> pure v
         Left _ -> expectationFailure "Server not started"
-    else started `shouldBe` True
+    else [serverStarted, fakeTargetStarted] `shouldBe` [True, True]
   where
     reduceCommunication socket com = do
       sendAll socket $ S.pack (comSend com)
       received <- S.unpack <$> recv socket 4096
       received `shouldBe` comReceived com
+    mult2Server socket = do
+      received <- S.unpack <$> recv socket 4096
+      sendAll socket $ S.pack $ [(received !! 0) * 2]
 
 connect :: Spec
 connect =
   describe "Simple connect request" $ do
-    it "Should send request" $ launchTest [Communication [5, 1, 0] [5, 0], Communication [5, 1, 0, 1, 216, 58, 215, 36, 0, 80] [5, 0, 0, 1, 216, 58, 215, 36, 0, 80]]
+    it "Should send CONNECT request and send payload with response" $ do
+      port <- freePort
+      let portInBinary = toWord8 port
+      launchTest
+        port
+        [ Communication [5, 1, 0] [5, 0], -- Negotiation
+          Communication ([5, 1, 0, 1, 127, 0, 0, 1] ++ portInBinary) ([5, 0, 0, 1, 127, 0, 0, 1] ++ portInBinary), -- CONNECT on 127.0.0.1
+          Communication [1] [2] -- payload
+        ]
+  where
+    freePort = getFreePort
+    toWord8 i = do
+      let weak = mod i 256 :: Int
+      let strong = (i - weak) `div` 256 :: Int
+      [fromIntegral strong, fromIntegral weak]
